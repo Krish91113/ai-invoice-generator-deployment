130e9c70e12ae536120bdc23b6715849c0884b8d
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-26 20:09:41 +0530

    Working

--- backend/routes/businessProfileRouter.js
+++ backend/routes/businessProfileRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import multer from "multer";
-import path from "path"
-import { createBusinessProfile, getMyBusinessProfile, updateBusinessProfile } from "../controllers/businessProfileController.js";
+import path from "path";
+import {
+  createBusinessProfile,
+  getMyBusinessProfile,
+  updateBusinessProfile,
+} from "../controllers/businessProfileController.js";
 
 const businessProfileRouter = express.Router();
 
@@ -10,35 +14,37 @@ businessProfileRouter.use(clerkMiddleware());
 
 //multer setup
 const storage = multer.diskStorage({
-    destination : (req, file, cb)=>{
-        cb(null, path.join(process.cwd(), "uploads"))
-    },
-    filename : (req, file, cb)=>{
-        const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        const ext = path.extname(file.originalname);
-        cb(null, `business-${unique}${ext}`)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: (req, file, cb) => {
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, `business-${unique}${ext}`);
+  },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
 //create eroute
-businessProfileRouter.post("/", 
-    upload.fields([
-        {name : "logoName", maxCount: 1},
-        {name : "stampName", maxCount: 1},
-        {name : "signatureNameMeta", maxCount: 1},
-    ]),
-    createBusinessProfile
-)
+businessProfileRouter.post(
+  "/",
+  upload.fields([
+    { name: "logoName", maxCount: 1 },
+    { name: "stampName", maxCount: 1 },
+    { name: "signatureNameMeta", maxCount: 1 },
+  ]),
+  createBusinessProfile
+);
 //update
-businessProfileRouter.put("/:id",
-    upload.fields([
-        {name : "logoName", maxCount: 1},
-        {name : "stampName", maxCount: 1},
-        {name : "signatureNameMeta", maxCount: 1},
-    ]),
-    updateBusinessProfile  
-)
-businessProfileRouter.get("/me", getMyBusinessProfile)
-export default businessProfileRouter
+businessProfileRouter.put(
+  "/:id",
+  upload.fields([
+    { name: "logoName", maxCount: 1 },
+    { name: "stampName", maxCount: 1 },
+    { name: "signatureNameMeta", maxCount: 1 },
+  ]),
+  updateBusinessProfile
+);
+businessProfileRouter.get("/me", getMyBusinessProfile);
+export default businessProfileRouter;
