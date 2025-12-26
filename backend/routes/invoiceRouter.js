fd0e28c37eedb5bcaa31f269e0741ba62ebbdbe8
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-26 14:39:21 +0530

    working

---
+++ backend/routes/invoiceRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { clerkMiddleware } from "@clerk/express";
+import { createInvoice, deleteInvoice, getInvoiceById, getInvoices, updateInvoice } from "../controllers/invoiceController.js";
+
+const invoiceRouter = express.Router();
+
+invoiceRouter.use(clerkMiddleware());
+
+invoiceRouter.use('/invoices', getInvoices);
+invoiceRouter.get('/:id', getInvoiceById);
+invoiceRouter.post('/', createInvoice);
+invoiceRouter.put('/:id', updateInvoice);
+invoiceRouter.delete('/:id', deleteInvoice);
+
+
+export default invoiceRouter;
