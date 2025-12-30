f9245b36800344947644073e64d65ab6d6a6866c
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-30 21:32:46 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,14 @@ import invoiceRouter from "./routes/invoiceRouter.js";
 import businessProfileRouter from "./routes/businessProfileRouter.js";
 import aiInvoiceRouter from "./routes/aiInvoiceRouter.js";
 
+
+//DB
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const _dirname =path.resolve();
 
 //MIDDLEWAREs
 app.use(
@@ -21,18 +27,23 @@ app.use(
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(clerkMiddleware());
-//DB
-connectDB();
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello from the backend server!");
-});
+
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/invoice", invoiceRouter);
 app.use("/api/businessProfile", businessProfileRouter);
 app.use("/api/ai", aiInvoiceRouter);
+
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.get(/.*/, (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
