249c1e4e948aae7f5813e302f51bae5b0646054b
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-30 10:34:22 +0530

    workig

--- frontend/src/component/KpiCard.jsx
+++ frontend/src/component/KpiCard.jsx
@@ -119,9 +119,15 @@ function KpiCard({
                     <path d="M23 6l-9.5 9.5-5-5L1 18" />
                     <path d="M17 6h6v6" />
                   </svg>
-
+{Math.abs(trend)}
                 </div>
               )}
+            </div>
+            <div className={kpiCardStyles.textContent}>
+              <div className={kpiCardStyles.title}>
+{title}
+              </div>
+              <div className={kpiCardStyles.value}>{value}</div>
             </div>
           </div>
         </div>
